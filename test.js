f068f45a786d3b2c04c01eb9a5e16b9d902bd82e
jonschlinkert <jon.schlinkert@sellside.com>
2017-10-30 10:26:00 -0400

    ensure that tag name is lowercase

--- test.js
+++ test.js
@@ -2,7 +2,7 @@
 
 require('mocha');
 var assert = require('assert');
-var isSelfClosing = require('..');
+var isSelfClosing = require('./');
 
 describe('is-self-closing', function() {
   it('should export a function', function() {
@@ -12,6 +12,11 @@ describe('is-self-closing', function() {
   it('should return true when name is a void or sgv element', function() {
     assert(isSelfClosing('img'));
     assert(isSelfClosing('path'));
+  });
+
+  it('should work with uppercase names', function() {
+    assert(isSelfClosing('IMG'));
+    assert(isSelfClosing('PATH'));
   });
 
   it('should return false when name is a not a void or svg element', function() {
